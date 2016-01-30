9b1eafd2a4a38fbd5856f077c20893cd2d1d4e83
Torrey Frith <theslcatf@gmail.com>
2016-01-30 17:57:16 -0500

    Implemented Enemy AI

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -2,14 +2,46 @@
 using System.Collections;
 
 public class EnemyController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
 	
+	public float health = 500;
+	public float speed = 500;
+	public int movementChangeTime=500;
+	
+	private int directionchange = 0;
+	private int horizontalMovement;
+	private int verticalMovement;
+	
+	void Update () {
+		if (directionchange == 0){
+			horizontalMovement=Random.Range(-1,2);
+			Debug.Log(horizontalMovement);
+			verticalMovement=Random.Range(-1,2);
+			Debug.Log(verticalMovement);
+			directionchange=movementChangeTime;
+		}else{
+			directionchange--;
+		}
+		
+		//initial valocity 0
+		Vector2 velocity = Vector2.zero;
+		//arrow keys change horizontal velocity
+		velocity += Vector2.right*horizontalMovement*speed*Time.deltaTime;
+		//arrow keys change vertical velocity
+		velocity += Vector2.up*-verticalMovement*speed*Time.deltaTime;
+		this.GetComponent<Rigidbody2D>().velocity = velocity; //set new velocity
+		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void OnTriggerEnter2D(Collider2D collider){
+		if (collider.gameObject.GetComponent<basicShot>()){
+			basicShot missile = collider.gameObject.GetComponent<basicShot>();
+			if(missile){
+				health -= missile.GetDamage();
+				missile.Hit();
+				if (health <= 0){
+					Destroy(gameObject);
+				}
+			}
+		}
 	}
 }
