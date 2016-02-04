088fb4d4d5ef0d894ce9ffa38fff6a3ee2659ef9
ZERGONTECH <anthonyluisp@hotmail.com>
2016-02-04 18:00:10 -0500

    Added Level Manager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadLevel(string name){
+        Debug.Log("New Level Load: " + name);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(name);
+    }
+    public void QuitRequest(){
+        Debug.Log("Quit Requested");
+        Application.Quit();
+    }
+
+}
