e91250a185ed6b3e7992db61c09ec03119cbb3eb
Carter Cook <13cjcc@gmail.com>
2016-02-27 11:43:09 -0500

    implemented sword knockback

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -4,38 +4,41 @@ using System;
 
 public class Sword : Weapon {
 
-    public int swingSpeed;
-    private int change;
-    private bool upSwing;
+	public int swingSpeed;
+	private int change;
+	private bool upSwing;
+	public float knockback;
 
-    void Start() {
-        change = 60;
-        upSwing = false;
-    }
+	void Start() {
+		change = 60;
+		upSwing = false;
+	}
 
-    public override void attack(Quaternion angle) {
+	public override void attack(Quaternion angle) {
 
-        if (change <= 0) {
-            upSwing = true;
-        } else if (change >= 70) {
-            upSwing = false;
-        }
+		if (change <= 0) {
+			upSwing = true;
+		} else if (change >= 70) {
+			upSwing = false;
+		}
 
-        transform.rotation = Quaternion.Euler(0, 0, angle.eulerAngles.z - change);
-        if (upSwing) {
-            change += swingSpeed;
-        } else {
-            change -= swingSpeed;
-        }
+		transform.rotation = Quaternion.Euler(0, 0, angle.eulerAngles.z - change);
+		if (upSwing) {
+			change += swingSpeed;
+		} else {
+			change -= swingSpeed;
+		}
 
-    }
+	}
 
-    void OnTriggerEnter2D(Collider2D collider) {
-        EnemyController enemy = collider.gameObject.GetComponent<EnemyController>();
-        if (enemy) {
-            enemy.hurt(damage);
-        }
-    }
+	void OnTriggerEnter2D(Collider2D collider) {
+		EnemyController enemy = collider.gameObject.GetComponent<EnemyController>();
+		if (enemy) {
+			enemy.hurt(damage);
+			Vector2 atEnemy = (enemy.transform.position - this.transform.position).normalized*knockback;
+			enemy.GetComponent<Rigidbody2D>().velocity += atEnemy; //apply knockback
+		}
+	}
 }
 
-    
+	
