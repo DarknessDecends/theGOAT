a9c0a9ea25f18349312a6116fb719a92f3facc04
Carter Cook <13cjcc@gmail.com>
2016-03-06 13:42:20 -0500

    dynamic boss music

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -8,15 +8,18 @@ public class MusicManager : MonoBehaviour {
 	private const float firstLoopStartTime = 2*2*spb; //2 bars * 2 beats per bar * bps
 	private const float firstLoopEndTime = (32*2+1)*spb; //(32 bars * 2 beats per bar + 1 beat because shostakovich is an asshole) * bps
 
+	private BossController boss;
 	// Use this for initialization
 	void Start () {
 		audioSource = GetComponent<AudioSource>();
+		boss = GameObject.FindObjectOfType<BossController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (audioSource.time >= firstLoopEndTime) {
+		if (!boss.detected && audioSource.time >= firstLoopEndTime) {
 			audioSource.time = firstLoopStartTime;
 		}
+		
 	}
 }
